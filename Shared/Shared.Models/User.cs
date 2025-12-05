15100ec52fe0c4039d3036fa9f64be4659eca044
asgermunk <75023079+asgermunk@users.noreply.github.com>
2025-12-05 18:22:43 +0100

    Implement user creation and retrieval functionality with MongoDB integration; update Docker and configuration files

--- Shared/Shared.Models/User.cs
+++ Shared/Shared.Models/User.cs
@@ -1,26 +1,44 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace FitnessApp.Shared.Models
 {
     public class User
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("_id")]
+        public string? Id { get; set; }
+        
         [Required]
-        public string UserId { get; set; } = string.Empty;
-        [Required]
+        [BsonElement("username")]
         public string Username { get; set; } = string.Empty;
+        
         [Required]
+        [BsonElement("firstName")]
         public string FirstName { get; set; } = string.Empty;
+        
         [Required]
+        [BsonElement("lastName")]
         public string LastName { get; set; } = string.Empty;
+        
         [Required]
+        [BsonElement("email")]
         public string Email { get; set; } = string.Empty;
+        
+        [BsonElement("licensePlate")]
         public string? LicensePlate { get; set; }
+        
+        [BsonElement("role")]
         public Role Role { get; set; } = Role.Member;
+        
+        [BsonElement("salt")]
         public string? Salt { get; set; }
+        
+        [BsonElement("hashedPassword")]
         public string? HashedPassword { get; set; }
-
-
     }
 }
