181590fb3c6b68e6b882a9dbca0eac03eb76dc46
asgermunk <75023079+asgermunk@users.noreply.github.com>
2025-12-10 21:38:30 +0100

    Implement FinishClass functionality with associated tests and ClassResult model

---
+++ ClassService/Model/ClassResult.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ClassService.Model
+{
+    public class ClassResult
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [BsonElement("_id")]
+        public string? Id { get; set; }
+        public string ClassId { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public Double CaloriesBurned { get; set; }
+        public Double Watt { get; set; }
+        public int DurationMin { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
