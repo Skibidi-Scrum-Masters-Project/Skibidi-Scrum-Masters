d7d90cccf91470fc47543a8ede4fb55d12545d81
Frederik <eaa23fogc@students.eaaa.dk>
2025-12-11 15:55:55 +0100

    Tilføjet required

--- SocialService/Models/Post.cs
+++ SocialService/Models/Post.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SocialService.Models;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
@@ -6,13 +8,17 @@ public class Post
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string? Id { get; set; }
     
     public int UserId { get; set; }
     public int FitnessClassId { get; set; }
     public int WorkoutId { get; set; }
     public DateTime PostDate { get; set; }
+    
+    [Required]
     public string PostTitle { get; set; }
+    
+    [Required]
     public string PostContent { get; set; }
     
     public List<Comment> Comments { get; set; } = new();
