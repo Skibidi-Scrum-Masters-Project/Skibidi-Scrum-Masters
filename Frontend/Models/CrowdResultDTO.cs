dda617499676ebe79afb3630ad85347f87581b2b
Schwennesen1 <eaa23besc@students.eaaa.dk>
2025-12-15 15:50:28 +0100

    update

--- Frontend/Models/CrowdResultDTO.cs
+++ Frontend/Models/CrowdResultDTO.cs
@@ -3,4 +3,20 @@ namespace FitLifeFitness.Models;
 public class CrowdResultDTO
 {
     
+    public string? Id { get; set; }
+        
+    public string UserId { get; set; } = string.Empty;
+    public DateTime EntryTime { get; set; }
+
+    public DateTime ExitTime { get; set; }
+
+    public timestatus Status { get; set; }
+
+    public enum timestatus
+    {
+        Entered,
+        Exited
+    }
+
+
 }
